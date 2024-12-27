2c44a3dd5824c9c7cc7cf7f3b3bd7cde5f250a58
Casan@LAPTOP-A5UFOJ80 <casanovag96@gmail.com>
2024-12-26 23:42:29 -0600

    Actualización de README.md y ajustes en configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-154ea1bf-a115-4e55-afb1-f7c04b238774.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-9ee3b5bf-afc2-45d0-8762-4f112a85d733.containerhub.tripleten-services.com"
 PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
 CREATE_USER_PATH = "/api/v1/users/"
